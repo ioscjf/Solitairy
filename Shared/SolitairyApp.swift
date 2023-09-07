74d00e2ac8dce51b0dc29158e242f4df4f8fb2b6
ioscjf <connor.fitzpatrick@onxmaps.com>
2023-09-07 13:16:20 -0500

    Test 1

--- Shared/SolitairyApp.swift
+++ Shared/SolitairyApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SolitairyApp: App {
     var body: some Scene {
         WindowGroup {
-            
+            //
             // test
             ContentView()
         }
