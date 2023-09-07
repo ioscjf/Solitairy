d3d09c4a1fbf403ea98ad5f65bd1b31ae6e5361a
Connor Fitzpatrick <connor.fitzpatrick@onxmaps.com>
2023-09-07 15:55:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,61 +4,43 @@ const fs = require("fs");
 const process = require("process");
 
 const { Octokit } = require("@octokit/rest");
-const globrex = require("globrex");
+const fetch = require("node-fetch");
 const Diff = require("diff");
 
 const defaultSizes = {
   0: "XS",
-  10: "S",
-  30: "M",
-  100: "L",
-  500: "XL",
-  1000: "XXL"
+  5: "S",
+  50: "M",
+  500: "L",
+  1000: "XL"
 };
 
 const actions = ["opened", "synchronize", "reopened"];
 
-const globrexOptions = { extended: true, globstar: true };
-
 async function main() {
-  debug("Running size-label-action...");
-
-  const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-  if (!GITHUB_TOKEN) {
-    throw new Error("Environment variable GITHUB_TOKEN not set!");
-  }
-
-  const GITHUB_EVENT_PATH = process.env.GITHUB_EVENT_PATH;
-  if (!GITHUB_EVENT_PATH) {
-    throw new Error("Environment variable GITHUB_EVENT_PATH not set!");
-  }
-
-  const eventDataStr = await readFile(GITHUB_EVENT_PATH);
+  const eventDataStr = await readFile(process.env.GITHUB_EVENT_PATH);
   const eventData = JSON.parse(eventDataStr);
 
   if (!eventData || !eventData.pull_request || !eventData.pull_request.base) {
-    throw new Error(`Invalid GITHUB_EVENT_PATH contents: ${eventDataStr}`);
+    throw new Error(`Invalid process.env.GITHUB_EVENT_PATH contents: ${eventDataStr}`);
   }
-
-  debug("Event payload:", eventDataStr);
 
   if (!actions.includes(eventData.action)) {
     console.log("Action will be ignored:", eventData.action);
     return false;
   }
 
-  const isIgnored = parseIgnored(process.env.IGNORED);
-
-  const pullRequestHome = {
-    owner: eventData.pull_request.base.repo.owner.login,
-    repo: eventData.pull_request.base.repo.name
-  };
-
-  const pull_number = eventData.pull_request.number;
+  const pullRequest = await octokit.rest.pulls.get({
+    owner: context.payload.repository.owner.login,
+    repo: context.payload.repository.name,
+    pull_number: context.payload.issue.number,
+  });
 
   const octokit = new Octokit({
-    auth: `token ${GITHUB_TOKEN}`,
-    userAgent: "pascalgn/size-label-action"
+    auth: process.env.CI_GITHUB_TOKEN,
+          request: {
+        fetch,
+      },
   });
 
   const pullRequestDiff = await octokit.pulls.get({
@@ -69,12 +51,9 @@ async function main() {
     }
   });
 
-  const changedLines = getChangedLines(isIgnored, pullRequestDiff.data);
-  console.log("Changed lines:", changedLines);
-
+  const changedLines = getChangedLines(pullRequestDiff.data);
   const sizes = getSizesInput();
   const sizeLabel = getSizeLabel(changedLines, sizes);
-  console.log("Matching label:", sizeLabel);
 
   const { add, remove } = getLabelChanges(
     sizeLabel,
@@ -87,7 +66,6 @@ async function main() {
   }
 
   if (add.length > 0) {
-    debug("Adding labels:", add);
     await octokit.issues.addLabels({
       ...pullRequestHome,
       issue_number: pull_number,
@@ -96,57 +74,16 @@ async function main() {
   }
 
   for (const label of remove) {
-    debug("Removing label:", label);
     try {
       await octokit.issues.removeLabel({
         ...pullRequestHome,
         issue_number: pull_number,
         name: label
       });
-    } catch (error) {
-      debug("Ignoring removing label error:", error);
     }
   }
-
-  debug("Success!");
 
   return true;
-}
-
-function debug(...str) {
-  if (process.env.DEBUG_ACTION) {
-    console.log.apply(console, str);
-  }
-}
-
-function parseIgnored(str = "") {
-  const ignored = str
-    .split(/\r|\n/)
-    .map(s => s.trim())
-    .filter(s => s.length > 0 && !s.startsWith("#"))
-    .map(s =>
-      s.length > 1 && s[0] === "!"
-        ? { not: globrex(s.substr(1), globrexOptions) }
-        : globrex(s, globrexOptions)
-    );
-  function isIgnored(path) {
-    if (path == null || path === "/dev/null") {
-      return true;
-    }
-    const pathname = path.substr(2);
-    let ignore = false;
-    for (const entry of ignored) {
-      if (entry.not) {
-        if (pathname.match(entry.not.regex)) {
-          return false;
-        }
-      } else if (!ignore && pathname.match(entry.regex)) {
-        ignore = true;
-      }
-    }
-    return ignore;
-  }
-  return isIgnored;
 }
 
 async function readFile(path) {
@@ -161,12 +98,9 @@ async function readFile(path) {
   });
 }
 
-function getChangedLines(isIgnored, diff) {
+function getChangedLines(diff) {
   return Diff.parsePatch(diff)
-    .flatMap(file =>
-      isIgnored(file.oldFileName) && isIgnored(file.newFileName)
-        ? []
-        : file.hunks
+    .flatMap(file => file.hunks
     )
     .flatMap(hunk => hunk.lines)
     .filter(line => line[0] === "+" || line[0] === "-").length;
